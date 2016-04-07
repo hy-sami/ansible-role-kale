b5d46a355a3353e20804ebb840a3952caf05c7c6
Sami Maisala <smaisala@helsinki.fi>
2016-04-07 12:09:05 +0300

    Renamed missed role name changes.

--- tests/launch-docker.sh
+++ tests/launch-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build --rm -t local/c7-systemd -f tests/Dockerfile .
-sudo docker run -it --privileged=true --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/ansible-role-nfs_mount:rw local/c7-systemd /bin/bash -c "/ansible-role-nfs_mount/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
+sudo docker run -it --privileged=true --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/ansible-role-kale:rw local/c7-systemd /bin/bash -c "/ansible-role-kale/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
